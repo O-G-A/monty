75655ac3908a3936cf723e1392f0fac552e9a9d1
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-08-24 07:33:50 -0700

    The nop opcode

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_nop- nothing
+ * @head: stack-head
+ * @counter: number of line
+ * Return: no
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
