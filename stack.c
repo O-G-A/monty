feac82299e7db6a66bbaab0b59a3166ce953d1d6
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-08-24 07:52:15 -0700

    The stack and queue opcodes

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - prints the top
+ * @head: stack-head
+ * @counter: number of line
+ * Return: no
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
