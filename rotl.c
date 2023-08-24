fa4a0cad5ec0b9a900b2805323fd7c4a2d240808
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-08-24 07:49:40 -0700

    The rotl opcode

---
+++ rotl.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * f_rotl- rotates stack to the top
+ * @head: stack-head
+ * @counter: number of line
+ * Return: no
+ */
+void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+{
+	stack_t *tmp = *head, *aux;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	aux = (*head)->next;
+	aux->prev = NULL;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = tmp;
+	(*head) = aux;
+}
